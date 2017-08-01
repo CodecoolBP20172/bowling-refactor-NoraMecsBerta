fd45f93c2c049f512bce0f199cbd0f096db236f1
Nora Mecs-Berta <ms.noraberta@gmail.com>
2017-08-02 00:25:40 +0200

    two new functions, one is count frame and change half, the other is handle the special characters if frame<10

--- bowling.py
+++ bowling.py
@@ -15,6 +15,26 @@ def get_value(char):
         raise ValueError()
 
 
+def ten_is_the_value(actual_char, next_char, second_next_char):
+    result = 0
+    if actual_char == '/':
+        result += get_value(next_char)
+    elif actual_char == 'X' or actual_char == 'x':
+        if second_next_char == '/':
+            result += 10 # get_value(game[character+1]) + 10 - get_value(game[character+1])
+        else:
+            result += get_value(next_char) + get_value(second_next_char)
+    return result
+
+def change_half_count_frame(frame, in_first_half, actual_char):
+    if not in_first_half:
+        frame += 1
+    in_first_half = not in_first_half # saját
+    if actual_char == 'X' or actual_char == 'x':
+        in_first_half = True
+        frame += 1
+    return frame, in_first_half
+
 
 def score(game):
     result = 0
@@ -23,40 +43,18 @@ def score(game):
     for character in range(len(game)):
         actual_char = game[character]
         previous_char = game[character-1]
-        #next_char = game[character+1]
-        #second_next_char = game[character+2]
-        value_of_previous = get_value(previous_char)
+        value_of_the_previous = get_value(previous_char)
         if actual_char == '/':
-            result += 10 - value_of_previous
+            result += 10 - value_of_the_previous
         else:
             result += get_value(actual_char)
-        if frame < 10 and get_value(actual_char) == 10:
-                if actual_char == '/':
-                    result += get_value(game[character+1])
-                elif actual_char == 'X' or actual_char == 'x':
-                    if game[character+2] == '/':
-                        result += 10 # get_value(game[character+1]) + 10 - get_value(game[character+1])
-                    else:
-                        result += get_value(game[character+1]) + get_value(game[character+2])
-        if not in_first_half:
-            frame += 1
-        in_first_half = not in_first_half # saját
-        if actual_char == 'X' or actual_char == 'x':
-            in_first_half = True
-            frame += 1
+        if frame < 10: # and get_value(actual_char) == 10: - saját rövidítés LEHET, H KELL
+            try:
+                result += ten_is_the_value(actual_char, game[character+1], game[character+2])
+            except IndexError:
+                continue
+        frame, in_first_half = change_half_count_frame(frame, in_first_half, actual_char)
     return result
 print(score('3/4x12'))
-'''
-def ten_is_the_value(game, result):
-    for character in range(len(game)):
-        actual_char = game[character]
-        previous_char = game[character-1]
-        if frame < 10 and get_value(actual_char) == 10:
-                if actual_char == '/':
-                    result += get_value(game[character+1])
-                elif actual_char == 'X' or actual_char == 'x':
-                    if game[character+2] == '/':
-                        result += 10 # get_value(game[character+1]) + 10 - get_value(game[character+1])
-                    else:
-                        result += get_value(game[character+1]) + get_value(game[character+2])
-'''
+
+
